e7e0bdd204104a1adf9e050b1288928cd1bf6a13
Mihai Stoica <41210245+MichaelVonWolfen@users.noreply.github.com>
2025-03-31 07:17:16 +0300

    Refactor AsuraScans worker to use Colly for web scraping
    Replaced manual HTTP requests with the Colly library to streamline web scraping of manga information and chapters. Updated dependencies in go.mod and go.sum to support the new functionality. Simplified error handling and improved code readability.

--- models/GlobalStructs.go
+++ models/GlobalStructs.go
@@ -2,24 +2,26 @@ package models
 
 type WorkerInformation struct {
 	WebsiteSearchUrl     string
+	BaseWebsiteUrl       string
 	MangaName            string
 	MangaId              int
 	MangaUrl             string
-	WebsiteSearchPattern string
+	ChapterSearchPattern string
 	MangaSearchPattern   string
 	ChapterList          []MangaChapter
 }
 type MangaInformation struct {
-	MangaId     int
-	Status      string
-	MangaName   string
-	MangaUrl    string
-	ChapterList []MangaChapter
+	MangaId       int
+	Status        string
+	MangaName     string
+	MangaUrl      string
+	MangaImageUrl string
+	ChapterList   []MangaChapter
 }
 type MangaChapter struct {
 	ChapterId     int
 	ChapterUrl    string
 	ChapterName   string
-	ChapterNumber float64
+	ChapterNumber string
 	MangaId       int
 }
